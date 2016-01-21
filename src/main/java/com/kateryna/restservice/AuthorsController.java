26510cdb0cb3d2f4ee3122978cbbd82e6e5d62c2
KatherineSh <katherine.shashkova@gmail.com>
2016-01-21 13:57:40 +0100

    init comit

---
+++ src/main/java/com/kateryna/restservice/AuthorsController.java
@@ -0,0 +1,14 @@
+package com.kateryna.restservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AuthorsController {
+
+	@RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String mainPage() {
+		return "index";
+	}
+}
